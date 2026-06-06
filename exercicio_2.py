cadceeede9969306ae48f4a4815fae0d97a72de5
Rubia082 <rubia082@gmail.com>
2026-06-06 12:52:19 -0300

    Update exercicio_2.py

--- exercicio_2.py
+++ exercicio_2.py
@@ -25,3 +25,14 @@ Pode-se utilizar o comando "while" ou até o "for" para te ajudar nisso.
 
 Fonte: Curso em vídeo.
 """
+
+numero= int(input("Digite um número:"))
+
+fatorial= 1
+contador= numero
+
+while contador > 0:
+    fatorial = fatorial * contador
+    contador = contador - 1
+
+print(f"O fatorial de {numero} é {fatorial}.")
